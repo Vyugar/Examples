0117e06c7d64752191ea145afdfa3c8d6e88d579
Vyugar <vyugar@yandex.ru>
2022-11-21 12:43:38 +0300

    aded Max

---
+++ Max/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 12;
+int b1 = 13;
+int c1 = 14;
+int a2 = 15;
+int b2 = 16;
+int c2 = 17;
+int a3 = 18;
+int b3 = 19;
+int c3 = 200;
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.WriteLine(max);
