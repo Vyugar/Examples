e28095f07c6850062f91e4a4545567625cd4d6e1
Vyugar <vyugar@yandex.ru>
2022-11-22 13:55:31 +0300

    aded Task006

---
+++ Task006/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+Console.WriteLine();
+}
+void selectionSort(int[]array)
+{
+    for (int i = 0; i < array.length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+            {
+                minPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(array);
+SelectionSort(array);
+PrintArray(array);
