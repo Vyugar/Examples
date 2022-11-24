e092a0a6a91367517bff8ac9bded9c55c8a5b9fd
Vyugar <vyugar@yandex.ru>
2022-11-24 21:13:26 +0300

    aded Task010

---
+++ Task010/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.Write("Введите число: ");
+string writeN = Console.ReadLine();
+int N = Convert.ToInt32(writeN);
+
+int GetFactorial(int N)
+{
+    int F = 1;
+    for (int num = 1; num <=N; num++)
+    {
+        F = F * num;
+    }
+    return F;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {GetFactorial(N)}");
+Console.WriteLine();
