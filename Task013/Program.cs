df274cf34886ffc8a5893d33e1148f17cf6679ac
Vyugar <vyugar@yandex.ru>
2022-12-01 16:48:24 +0300

    aded Task013

---
+++ Task013/Program.cs
@@ -0,0 +1,12 @@
+﻿double[,] array = new double[5, 10];
+
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        array[i, j] = random.NextDouble() * 100; // TODO: Все дело в математике
+        Console.Write(array[i, j].ToString("F3") + "\t");
+    }
+    Console.WriteLine();
+}
